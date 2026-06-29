16872f1a93eef15ca69a0d3d3df8ba279b8e7316
Masud Alam <masud.alam.it2003@gmail.com>
2026-06-29 23:01:08 +0530

    cat and dog age to human age formula

--- 01-tweetCount/index.js
+++ 01-tweetCount/index.js
@@ -1,10 +1,9 @@
 // To find out how many characters have been written and remained in tweet.
-/*
+
 const tweet = prompt("Write your message!");
 const characterCount = tweet.length;
 const characterRemain = 280 - characterCount;
 alert(`You have written ${characterCount} characters. You have ${characterRemain} characters remain.`)
-*/
 
 // Slicing the tweet 
 const tweet = prompt("Compose your tweet here!")
@@ -13,3 +12,13 @@ alert(tweetUnder280);
 
 // In one line 
 alert(prompt("Compose your tweet!").slice(0,280));
+
+// Capitalize the first letter
+const name = prompt("Enter your name!");
+sliceFirstChar = name.slice(0,1);
+capitalFirstChar = sliceFirstChar.toUpperCase();
+smallRestChar = name.slice(1,length.name).toLowerCase();
+orderedName = capitalFirstChar + smallRestChar;
+alert("Hello, " + orderedName);
+
+
---
+++ 02-CatAge-toHumanAge/index.js
@@ -0,0 +1,11 @@
+// Dog age to human age formula (what if dog will be human?)
+// humanAge = (dogAge - 2)* 4 + 12
+dogAge = prompt("How old is your dog?");
+humanAge = (dogAge - 2)* 4 + 12
+alert(`Your dog is ${humanAge} years old in human age.`);
+
+// catAge to humanAge
+// Formula fur over two years old
+catAge = prompt("Enter your Cat age!");
+humanAge = (catAge - 2)* 4 + 24
+alert(`Your cat is ${humanAge} years old in human.`);
