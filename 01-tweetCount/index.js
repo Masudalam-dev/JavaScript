1e8cd3b3d489491d2c1b0edb029cc2778220876f
Masud Alam <masud.alam.it2003@gmail.com>
2026-06-29 21:40:42 +0530

    added some more features like slice the tweet

--- 01-tweetCount/index.js
+++ 01-tweetCount/index.js
@@ -1,6 +1,15 @@
 // To find out how many characters have been written and remained in tweet.
-
+/*
 const tweet = prompt("Write your message!");
 const characterCount = tweet.length;
 const characterRemain = 280 - characterCount;
 alert(`You have written ${characterCount} characters. You have ${characterRemain} characters remain.`)
+*/
+
+// Slicing the tweet 
+const tweet = prompt("Compose your tweet here!")
+tweetUnder280 = tweet.slice(0,280);
+alert(tweetUnder280);
+
+// In one line 
+alert(prompt("Compose your tweet!").slice(0,280));
